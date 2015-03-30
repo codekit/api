9da74429f4754179ee911a61cf8a795e14b9f970
avireni <beena@avireni.com>
2015-03-30 15:25:54 -0400

    added scripts and styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,37 @@
 
 $(document).ready(function () {
     $('#results').html('');
+
+
+    function itemHover(item) {
+
+        var itemHover = $('.hover')[0].src;
+        $('#heroimage').append('<img src=' + itemHover + ' alt="photo" > ');
+        var itemdesc = $('p.desc').html();
+        $('#herodesc').append('<p>' + itemdesc + '</p>');
+        var itemprice = $('p.priceTag').html();
+        $('#heroprice').append('<p class="prodprice">' + itemprice + '</p>');
+        var itemArray = $('.itemList');
+        itemArray.each(function (i) {
+            $(this).mouseenter(function () {
+                $('#heroimage').html('');
+                $('#herodesc').html('');
+                $('#heroprice').html('');
+                var bigImage = $('.hover')[i].src;
+                $('#heroimage').append('<img src=' + bigImage + ' alt="photo" > ');
+                var itemDesc = $('p.desc').html();
+                $('#herodesc').append('<p>' + itemDesc + '</p>');
+                var itemPrice = $('p.priceTag').html();
+                $('#heroprice').append('<p>' + itemPrice + '</p>');
+
+            });
+        });
+    }
+    $('#addcart').click(function () {
+        var productPrice = $('.prodprice').html();
+        alert("Price is" + productPrice);
+    });
+
     $.ajax({
         type: 'GET',
         url: 'http://m.lowes.com/IntegrationServices/resources/productList/json/v3_0/4294857975?langId=-1&storeId=10702&catalogId=10051&nValue=4294857975&storeNumber=0595&pageSize=20&firstRecord=0&refinements=5003703',
@@ -32,13 +63,15 @@ $(document).ready(function () {
                 var productImagelg = result.ProductsList[i].imageURLs.lg;
                 var productSpecs = result.ProductsList[i].ProductInfo.p_product_specs;
                 var viewBtn = '<div class="viewBtn">View More</div>';
-                $('#results').append('<li> <img src=' + productImageSm + ' alt="photo" class="hover"> <p> ' + productBrand + ' ' + productdesc + ' </p> <p class="priceTag"> $ ' + productPrice + ' </p>' + viewBtn + '</li>');
-
+                $('#results').append('<li class="itemList"> <img src=' + productImageSm + ' alt="photo" class="itemImage"> <img src=' + productImagelg + ' class="hover"> <p class="desc"> ' + productBrand + ' ' + productdesc + ' </p> <p class="priceTag"> $ ' + productPrice + ' </p>' + viewBtn + '</li>');
 
                 console.log(productBrand);
+
+
             }
-            $('#hero').append('<img src=' + productImagelg + ' alt="photo" > ');
+            itemHover(i);
         },
+
         error: function () {}
     });
 });
